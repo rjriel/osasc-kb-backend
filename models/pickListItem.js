631785c86eccb6056a8af214a6e7bc8b6b396ba1
noodledpirate <noodledpirate>
2018-10-13 19:53:52 -0400

    Fix assignment issue for PickListItem

--- models/pickListItem.js
+++ models/pickListItem.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var pickListItemSchema = new Schema({
   name:  'String',
-  pickListTypeId = 'ObjectId',
+  pickListTypeId : 'ObjectId',
 });
 
 module.exports = mongoose.model('pickListItem', pickListItemSchema);
