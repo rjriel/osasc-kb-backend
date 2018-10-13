cc7da5ee68ca83c01f69023865ebaeda8fbbe0f3
Tai Dickerson <tdickers@akamai.com>
2018-10-13 14:53:34 -0400

    Add basic auth example

--- auth/noop.js
+++
@@ -1,9 +0,0 @@
-var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if (err) { return done(err); }
-    return done(null);
-  }
-));
--- index.js
+++ index.js
@@ -8,19 +8,39 @@ mongoose.set("debug", true)
 mongoose.connect(config.connectionString);
 
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    return done(null, true);
+  }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+  });
+  
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
 
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Note: sessions can be stored in Mongo
+app.use(require('express-session')({ secret: config.sessionSecret }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/knowledge/', knowledgeItemRouter)
-app.post('/authExample', passport.authenticate('local'),
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/auth/example/', passport.authenticate('local'),
 function(req, res) {
   // If this function gets called, authentication was successful.
   // `req.user` contains the authenticated user.
-  res.redirect('/users/' + req.user.username);
+  res.send('Hello Authenticated User!')
 });
+
+app.use('/knowledge/', knowledgeItemRouter)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
