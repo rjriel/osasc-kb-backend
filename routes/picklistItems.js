0387df9eb6b563ec2d5c1a80484ff64d3631b142
Tai Dickerson <tdickers@akamai.com>
2018-10-13 20:54:46 -0400

    Fix another typo

--- routes/picklistItems.js
+++ routes/picklistItems.js
@@ -1,7 +1,7 @@
 const express= require('express')
 const router = express.Router()
 
-const PicklistItem = require('../models/pickistItem')
+const PicklistItem = require('../models/picklistItem')
 
 router.delete('/:id', function(req, res) {
 	
